6b23da888db3e59a019e099beb16e5c311bf97ae
= <=>
2022-01-18 17:24:51 +0000

    Responsive de la page galerie

--- galerie.js
+++ galerie.js
@@ -1,7 +1,8 @@
 
 // Insertion des images par une boucle
 
-console.log(document.querySelector(".item:nth-child(2)"));
 for(let i = 1; i<13; i++){
     document.querySelector(`.item:nth-child(${i})`).style.backgroundImage = `url(images/images-portfolio/img-portfolio${i}.jpg)`
 }
+
+// Affichage de la galerie
